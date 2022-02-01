7ac51386b58991890542c2d071370d0ddacbb313
bhhicks221 <90063321+bhhicks221@users.noreply.github.com>
2022-01-31 20:38:49 -0600

    initial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Programming_Assignments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
